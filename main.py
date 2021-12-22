f656325ece0d556c2a63e30046f90afaf6b48bee
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-12-22 15:18:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,84 +2,69 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 import time
 
+ACCOUNT_EMAIL = "EMAIL"
+ACCOUNT_PWD = "PASSWORD"
+PHONE = "NUMBER"
 
-ACCOUNT_EMAIL = ""
-ACCOUNT_PWD = ""
-PHONE = ""
-
-service = Service("")
+service = Service("SERVICE")
 driver = webdriver.Chrome(service=service)
-driver.get("")
+driver.get(
+    "URL")
 
 sign_in_button = driver.find_element(By.XPATH, "/html/body/div[1]/header/nav/div/a[2]")
 sign_in_button.click()
-
 time.sleep(2)
-
 email_field = driver.find_element(By.ID, "username")
 email_field.send_keys(ACCOUNT_EMAIL)
 password_field = driver.find_element(By.ID, "password")
 password_field.send_keys(ACCOUNT_PWD)
 password_field.send_keys(Keys.ENTER)
-
-time.sleep(1)
-
-# all_listings = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
-all_listings = driver.find_elements(By.CSS_SELECTOR, "div.job-card-container")
-# print(len(all_listings))
-
-for listing in all_listings:
-    print("called")
-    listing.click()
-    time.sleep(2)
-
-    # Try to locate the apply button, if can't locate then skip the job.
-    try:
-        apply_button = driver.find_element(By.CSS_SELECTOR, "div div div div button")
-        apply_button.click()
-        time.sleep(2)
-
-        next_button = driver.find_element(By.CSS_SELECTOR, "[aria-label=Continue to next step]")
-        next_button.click()
-        time.sleep(2)
-
-        review_button = driver.find_element(By.CSS_SELECTOR, "[aria-label=Review your Application]")
-        review_button.click()
-        time.sleep(2)
-
-        # # If phone field is empty, then fill your phone number.
-        # phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
-        # if phone.text == "":
-        #     phone.send_keys(PHONE)
-
-        submit_button = driver.find_element(By.CSS_SELECTOR, "[aria-label=Submit application]")
-        submit_button.click()
-        time.sleep(2)
-
-#         # If the submit_button is a "Next" button, then this is a multi-step application, so skip.
-#         if submit_button.get_attribute("data-control-name") == "continue_unify":
-#             close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
-#             close_button.click()
-#             time.sleep(2)
-#             discard_button = driver.find_elements(By.CLASS_NAME, "artdeco-modal__confirm-dialog-btn")[1]
-#             discard_button.click()
-#             print("Complex application, skipped.")
-#             continue
-#         else:
-#             submit_button.click()
-#
-#         # Once application completed, close the pop-up window.
-#         time.sleep(2)
-#         close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
-#         close_button.click()
-#
-    # If already applied to job or job is no longer accepting applications, then skip.
-    except NoSuchElementException:
-        print("No application button, skipped.")
-        continue
-
 time.sleep(2)
-driver.quit()
+
+
+def make_apply(com):
+    com.click()
+    time.sleep(1)
+    try:
+        # driver.find_element_by_class_name('jobs-apply-button').click()
+        driver.find_element(By.CSS_SELECTOR,
+                            ".jobs-unified-top-card__content--two-pane button.jobs-apply-button").click()
+        time.sleep(1)
+        driver.find_element(By.CSS_SELECTOR, "form .artdeco-button--primary").click()
+        time.sleep(1)
+        driver.find_element(By.CSS_SELECTOR, "form .artdeco-button--primary").click()
+        time.sleep(1)
+        driver.find_element(By.CSS_SELECTOR, ".jobs-easy-apply-content .artdeco-button--primary").click()
+        time.sleep(1)
+
+        try:
+            driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss").click()
+            time.sleep(1)
+            driver.find_element(By.CSS_SELECTOR, ".artdeco-modal__actionbar .artdeco-button--primary").click()
+        except NoSuchElementException:
+            print('Applied!')
+            try:
+                driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss").click()
+            except NoSuchElementException:
+                print('There was no questions')
+        else:
+            print('Filling out a form is requied!')
+
+    except NoSuchElementException:
+        print('Already applied')
+    except ElementNotInteractableException:
+        print('LinkedIn application loading error')
+
+
+companies = driver.find_elements(By.CLASS_NAME, "job-card-container--clickable")
+print(len(companies))
+# make_apply(companies[4])
+
+for company in companies:
+    make_apply(company)
+    time.sleep(1)
+
+driver.close()
